3fe2f0ed51b96ab5230232c5c08d4c283cfc2193
MHealton <markhealton87@gmail.com>
2020-10-29 22:45:42 -0700

    optimize images, configure carousel

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+	slidesToScroll: 1,
+  draggable: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
